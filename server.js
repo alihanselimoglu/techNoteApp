fa3d69c83a678d2029021a18567b75b36e5d5b59
Alihan <82150661+alienslimeson@users.noreply.github.com>
2023-08-13 13:39:29 +0300

    MongoDB

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
-const { logger } = require('./middleware/logger');
+const { logger, logEvents } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
+const connectDB = require('./config/dbConn');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3500;
+
+connectDB();
 
 //middleware
 app.use(express.json());
@@ -30,6 +35,14 @@ app.all('*', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}...`);
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready....');
+  app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+  });
 });
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
