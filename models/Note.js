847d5fdc4ca671dd0403ecdcd93303ef0e974b83
Alihan <82150661+alienslimeson@users.noreply.github.com>
2023-08-15 18:32:57 +0300

    note api

--- models/Note.js
+++ models/Note.js
@@ -1,36 +1,35 @@
-const mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
+const mongoose = require('mongoose')
+const AutoIncrement = require('mongoose-sequence')(mongoose)
 
 const noteSchema = new mongoose.Schema(
-  {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'User'
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        text: {
+            type: String,
+            required: true
+        },
+        completed: {
+            type: Boolean,
+            default: false
+        }
     },
-
-    title: {
-      type: String,
-      required: true,
-    },
-
-    text: {
-      type: String,
-      required: true,
-    },
-
-    completed: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  { timestamps: true }
-);
+    {
+        timestamps: true
+    }
+)
 
 noteSchema.plugin(AutoIncrement, {
-  inc_field: 'ticket',
-  id: 'ticketNums',
-  start_seq: 500,
-});
+    inc_field: 'ticket',
+    id: 'ticketNums',
+    start_seq: 500
+})
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model('Note', noteSchema)
--- server.js
+++ server.js
@@ -22,6 +22,7 @@ app.use(logger);
 
 app.use('/', require('./routes/root'));
 app.use('/users', require('./routes/userRoutes'));
+app.use('/notes', require('./routes/noteRoutes'));
 
 app.all('*', (req, res) => {
   res.status(404);
